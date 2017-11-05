bf93b5d97a1179d6326bb35d164b530cdc5053a3
Mohd Danish <danish29azmi@gmail.com>
2017-11-05 23:03:20 +0530

    This design can be implemented on any FPGA.
    UCF file needed to implemented this design to any FPGA

---
+++ Syn_FIFO.v
@@ -0,0 +1,113 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Danish Azmi
+// 
+// Create Date:    21:42:56 11/05/2017 
+// Design Name: Synchronus FIFO
+// Module Name:    Syn_FIFO 
+// Project Name: FIFO Design
+// Target Devices: None
+// Tool versions: ISE 14.2
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`define BUF_WIDTH 3    // BUF_SIZE = 16 -> BUF_WIDTH = 4, no. of bits to be used in pointer
+`define BUF_SIZE ( 1<<`BUF_WIDTH )
+
+module Sync_FIFO( clk, rst, buf_in, buf_out, wr_en, rd_en, buf_empty, buf_full, fifo_counter );
+
+input                 rst, clk, wr_en, rd_en;   
+// reset, system clock, write enable and read enable.
+input [7:0]           buf_in;                   
+// data input to be pushed to buffer
+output[7:0]           buf_out;                  
+// port to output the data using pop.
+output                buf_empty, buf_full;      
+// buffer empty and full indication 
+output[`BUF_WIDTH :0] fifo_counter;             
+// number of data pushed in to buffer   
+
+reg[7:0]              buf_out;
+reg                   buf_empty, buf_full;
+reg[`BUF_WIDTH :0]    fifo_counter;
+reg[`BUF_WIDTH -1:0]  rd_ptr, wr_ptr;           // pointer to read and write addresses  
+reg[7:0]              buf_mem[`BUF_SIZE -1 : 0]; //  
+
+always @(fifo_counter)
+begin
+   buf_empty = (fifo_counter==0);   // Checking for whether buffer is empty or not
+   buf_full = (fifo_counter== `BUF_SIZE);  //Checking for whether buffer is full or not
+
+end
+
+//Setting FIFO counter value for different situations of read and write operations.
+always @(posedge clk or posedge rst)
+begin
+   if( rst )
+       fifo_counter <= 0;		// Reset the counter of FIFO
+
+   else if( (!buf_full && wr_en) && ( !buf_empty && rd_en ) )  //When doing read and write operation simultaneously
+       fifo_counter <= fifo_counter;			// At this state, counter value will remain same.
+
+   else if( !buf_full && wr_en )			// When doing only write operation
+       fifo_counter <= fifo_counter + 1;
+
+   else if( !buf_empty && rd_en )		//When doing only read operation
+       fifo_counter <= fifo_counter - 1;
+
+   else
+      fifo_counter <= fifo_counter;			// When doing nothing.
+end
+
+always @( posedge clk or posedge rst)
+begin
+   if( rst )
+      buf_out <= 0;		//On reset output of buffer is all 0.
+   else
+   begin
+      if( rd_en && !buf_empty )
+         buf_out <= buf_mem[rd_ptr];	//Reading the 8 bit data from buffer location indicated by read pointer
+
+      else
+         buf_out <= buf_out;		
+
+   end
+end
+
+always @(posedge clk)
+begin
+   if( wr_en && !buf_full )
+      buf_mem[ wr_ptr ] <= buf_in;		//Writing 8 bit data input to buffer location indicated by write pointer
+
+   else
+      buf_mem[ wr_ptr ] <= buf_mem[ wr_ptr ];
+end
+
+always@(posedge clk or posedge rst)
+begin
+   if( rst )
+   begin
+      wr_ptr <= 0;		// Initializing write pointer
+      rd_ptr <= 0;		//Initializing read pointer
+   end
+   else
+   begin
+      if( !buf_full && wr_en )    
+			wr_ptr <= wr_ptr + 1;		// On write operation, Write pointer points to next location
+      else  
+			wr_ptr <= wr_ptr;
+
+      if( !buf_empty && rd_en )   
+			rd_ptr <= rd_ptr + 1;		// On read operation, read pointer points to next location to be read
+      else 
+			rd_ptr <= rd_ptr;
+   end
+
+end
+endmodule
---
+++ Test_Syn_FIFO.v
@@ -0,0 +1,133 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   21:45:16 11/05/2017
+// Design Name:   Test Synchronus FIFO
+// Module Name:   E:/CDAC Work/CDAC/XilinxISE_danish/Synchronus_FIFO/Test_Syn_FIFO.v
+// Project Name:  Synchronus_FIFO
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: fifo
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+`define BUF_WIDTH 3
+
+module Test_Sync_FIFO();
+reg clk, rst, wr_en, rd_en ;
+reg[7:0] buf_in;
+reg[7:0] tempdata;
+wire [7:0] buf_out;
+wire [`BUF_WIDTH :0] fifo_counter;
+
+Sync_FIFO ff( .clk(clk), .rst(rst), .buf_in(buf_in), .buf_out(buf_out), 
+         .wr_en(wr_en), .rd_en(rd_en), .buf_empty(buf_empty), 
+         .buf_full(buf_full), .fifo_counter(fifo_counter) );
+
+initial
+begin
+   clk = 0;
+   rst = 1;
+        rd_en = 0;
+        wr_en = 0;
+        tempdata = 0;
+        buf_in = 0;
+
+
+        #15 rst = 0;
+  
+        push(1);
+        fork
+           push(2);
+           pop(tempdata);
+        join              //push and pop together   
+        push(10);
+        push(20);
+        push(30);
+        push(40);
+        push(50);
+        push(60);
+        push(70);
+        push(80);
+        push(90);
+        push(100);
+        push(110);
+        push(120);
+        push(130);
+
+        pop(tempdata);
+        push(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+		  push(140);
+        pop(tempdata);
+        push(tempdata);//
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        pop(tempdata);
+        push(5);
+        pop(tempdata);
+end
+
+always
+   #5 clk = ~clk;
+
+task push;
+input[7:0] data;
+
+
+   if( buf_full )
+            $display("---Cannot push: Buffer Full---");
+        else
+        begin
+           $display("Pushed ",data );
+           buf_in = data;
+           wr_en = 1;
+                @(posedge clk);
+                #1 wr_en = 0;
+        end
+
+endtask
+
+task pop;
+output [7:0] data;
+
+   if( buf_empty )
+            $display("---Cannot Pop: Buffer Empty---");
+   else
+        begin
+
+     rd_en = 1;
+          @(posedge clk);
+
+          #1 rd_en = 0;
+          data = buf_out;
+           $display("-------------------------------Poped ", data);
+
+        end
+endtask
+
+endmodule
+
+
